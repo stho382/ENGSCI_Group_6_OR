ee77dc4640da955794e730b1a12031666ad8e314
bhal447 <nzben2000@gmail.com>
2021-10-03 20:45:20 +1300

    Update

--- LinearProgram.py
+++ LinearProgram.py
@@ -3,6 +3,21 @@ import pandas as pd # We will discuss this more next week!
 from pulp import *
 from routeGeneration import *
 
+max_routes = 30 * 6
+
 possible_routes = [i for i in range(0, len(routes))]
 
 x = LpVariable.dicts("Route", possible_routes, 0, None, LpBinary)
+
+prob += lpSum[(225 * x[route] * hours[route] for route in possible_routes)]
+
+prob += lpSum[(x[route] for route in possible_routes)] <= max_routes
+
+for store in stores:
+    prob += lpSum([x[route]] for route in possible_routes if store in routes[route]) == 1
+
+prob.writeLP("Woolworths.lp")
+
+prob.solve()
+
+print("Status:", LPStatus[prob.status])
--- OR_Project.py
+++ OR_Project.py
@@ -7,16 +7,11 @@ stores = stores[1:67]
 stores = np.delete(stores, 55, 0)
 
 # Array of stores in central north region
-LocationCentralNorth_stores = np.genfromtxt('LocationCentralNorth.csv', dtype = str, delimiter= ',', skip_header=1, usecols=0)
+CentralNorth_stores = np.genfromtxt('LocationCentralNorth.csv', dtype = str, delimiter= ',', skip_header=1, usecols=0)
 
-# Find median demands of each day for every store
-LocationCentralNorth_demands_monday = np.genfromtxt('LocationCentralNorth.csv', dtype = str, delimiter= ',', skip_header=1, usecols=1)
-LocationCentralNorth_demands_tuesday = np.genfromtxt('LocationCentralNorth.csv', dtype = str, delimiter= ',', skip_header=1, usecols=2)
-LocationCentralNorth_demands_wednesday = np.genfromtxt('LocationCentralNorth.csv', dtype = str, delimiter= ',', skip_header=1, usecols=3)
-LocationCentralNorth_demands_thursday = np.genfromtxt('LocationCentralNorth.csv', dtype = str, delimiter= ',', skip_header=1, usecols=4)
-LocationCentralNorth_demands_friday = np.genfromtxt('LocationCentralNorth.csv', dtype = str, delimiter= ',', skip_header=1, usecols=5)
-LocationCentralNorth_demands_saturday = np.genfromtxt('LocationCentralNorth.csv', dtype = str, delimiter= ',', skip_header=1, usecols=6)
-
+# Find median demands for weekdays for every store
+CentralNorth_WeekdayDemands = np.genfromtxt('LocationCentralNorth.csv', delimiter= ',', skip_header=1, usecols=1)
+'''
 # Time from each store to each store
 travel_durations = np.genfromtxt('WoolworthsTravelDurations.csv', dtype = str, delimiter= ',', skip_header= 1, usecols= list(range(1,67)))
 
@@ -28,56 +23,32 @@ distribution_time = np.genfromtxt('WoolworthsTravelDurations.csv', dtype = str, 
 
 # Remove distribution centre
 distribution_time = np.delete(distribution_time, 55, 0)
+'''
 
-time_threshold = 60 * 4 * 2 * 30 
+#time_threshold = 60 * 4 * 2 * 30 
 demand_threshold = 26
 
 routes = []
-hours = []
+#hours = []
 
-time = 0
+#time = 0
 demand = 0
 
-for i in range(len(stores)):
+for i in range(len(CentralNorth_stores)):
     smallest_route_visited = []
     smallest_route_visited.append(i)
 
-    for ii in range():
+    for j in range(len(CentralNorth_stores)):
         next_store = 0
-        time = 0
-        demand = 0
+        demand = CentralNorth_WeekdayDemands[i]
         route_list = []
         nodes_list = []
 
-        duration_store = travel_durations[i]
-        time = distribution_time[i] + (7.5*60*demands)
-        route_list.append(stores[i])
-        nodes_list.append(i)
-        demand += demands[i]
+        route_list.append(CentralNorth_stores[i])
 
         visited = False
         thisSmallest = False
 
-        for j in range(len(duration_store)):
-            thisSmallest = j in smallest_route_visited
-            if (thisSmallest == True):
-                duration_store[j] = 0
-
-
-
-
-            if ((time < time_threshold) & (demand < demand_threshold)):
-                back_home = distribution_time[next_store]
-                time -= distribution_time[next_store]
-                route_list.append(stores[next_store])
-                duration_store = travel_durations[next_store]
-            else:
-                time -= duration_store[next_store] + (7.5 * 60 * demands[next_store] + distribution_time[next_store])
-                time += back_home
-                back_home = 0
-
-    time = time / (60*60)
-    time = (math.ceil(time*4)) / 4
-    routes.append(route_list)
-    hours.append(time)
-
+        while (demand < demand_threshold):
+            if i != j:
+                route_list
