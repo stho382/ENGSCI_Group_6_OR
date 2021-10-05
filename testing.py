c258c1b46a4ff09d57337eb451ac3eaf16e984d8
serenayu511 <80793832+serenayu511@users.noreply.github.com>
2021-10-05 22:44:16 +1300

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,6 @@
+from OR_Project import centralnorthroutesfunction
+CentralNorth_Routes3 = (list(combinations(CentralNorth_stores, 3)))
+CentralNorth_Routes4 = (list(combinations(CentralNorth_stores, 4)))
+CentralNorth_Routes5 = (list(combinations(CentralNorth_stores, 5)))
+
+centralnorthroutesfunction(CentralNorth_Routes3, CentralNorth_Routes4, CentralNorth_Routes5)
